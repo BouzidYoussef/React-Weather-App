660b1ddfd5539633fdd0fec73450303dff4a8abd
BouzidYoussef <bouzidyoussef600@yahoo.com>
2022-08-20 23:35:04 +0100

    font set

---
+++ react-weather-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Search from './components/search'
+
+function App() {
+  return (
+    <div className="container">
+      <Search />
+    </div>
+  );
+}
+
+export default App;
